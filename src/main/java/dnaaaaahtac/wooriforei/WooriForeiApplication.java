d762792fe58404b493d2014c8f0be336b61d9576
김은채 <ke808762@gmail.com>
2024-04-07 16:58:01 +0900

    add: 초기 세팅

---
+++ src/main/java/dnaaaaahtac/wooriforei/WooriForeiApplication.java
@@ -0,0 +1,13 @@
+package dnaaaaahtac.wooriforei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriForeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooriForeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dnaaaaahtac/wooriforei/WooriForeiApplicationTests.java
@@ -0,0 +1,13 @@
+package dnaaaaahtac.wooriforei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriForeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
