8c7bff01055f03f1adb78b9de0a61889f2caab96
de123456sdf <cdaeun95@gmail.com>
2024-04-09 22:16:06 +0900

    fix: Repository 어노테이션 추가

--- src/main/java/dnaaaaahtac/wooriforei/domain/openapi/repository/OpenApiRepository.java
+++ src/main/java/dnaaaaahtac/wooriforei/domain/openapi/repository/OpenApiRepository.java
@@ -2,6 +2,8 @@ package dnaaaaahtac.wooriforei.domain.openapi.repository;
 
 import dnaaaaahtac.wooriforei.domain.openapi.entity.Information;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OpenApiRepository extends JpaRepository<Information, Long> {
 }
