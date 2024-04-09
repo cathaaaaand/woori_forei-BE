778b7ecc074ac7de46b5631c7ac23375d98f7b41
de123456sdf <cdaeun95@gmail.com>
2024-04-09 22:14:54 +0900

    fix: 인증키 매개변수로 변경

--- src/main/java/dnaaaaahtac/wooriforei/domain/openapi/service/InformationService.java
+++ src/main/java/dnaaaaahtac/wooriforei/domain/openapi/service/InformationService.java
@@ -2,6 +2,7 @@ package dnaaaaahtac.wooriforei.domain.openapi.service;
 
 import dnaaaaahtac.wooriforei.domain.openapi.dto.InformationResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -10,17 +11,20 @@ import reactor.core.publisher.Mono;
 public class InformationService {
 
     private final WebClient webClient;
+    private final String authKey;
 
     @Autowired
-    public InformationService(WebClient.Builder webClientBuilder) {
+    public InformationService(WebClient.Builder webClientBuilder,
+                              @Value("${AUTH_KEY}") String authKey) {
         this.webClient = webClientBuilder.baseUrl("http://openapi.seoul.go.kr:8088").build();
+        this.authKey = authKey;
     }
 
     public Mono<InformationResponseWrapper> retrieveInformation() {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/{authKey}/{responseType}/{serviceName}/{startIdx}/{endIdx}")
-                        .build("4f52525a70636461313334534163676c", "json", "TbTourInformation", 1, 999))
+                        .build(authKey, "json", "TbTourInformation", 1, 999))
                 .header("Content-type", "application/json")
                 .retrieve()
                 .bodyToMono(InformationResponseWrapper.class)
