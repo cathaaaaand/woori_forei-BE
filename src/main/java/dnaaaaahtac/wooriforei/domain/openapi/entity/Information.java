1ccd71ba5cfa0430c3c2eddb3f76e87cb9e61385
de123456sdf <cdaeun95@gmail.com>
2024-04-13 22:29:46 +0900

    fix: 안쓰는 import 삭제

--- src/main/java/dnaaaaahtac/wooriforei/domain/openapi/entity/Information.java
+++ src/main/java/dnaaaaahtac/wooriforei/domain/openapi/entity/Information.java
@@ -1,6 +1,5 @@
 package dnaaaaahtac.wooriforei.domain.openapi.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
