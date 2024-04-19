2e44d0225a7e95876177a8c0463dd44d5bdd43dd
de123456sdf <cdaeun95@gmail.com>
2024-04-20 00:40:16 +0900

    feature: aws 앤드포인트 추가

---
+++ src/main/java/dnaaaaahtac/wooriforei/domain/auth/controller/AwsController.java
@@ -0,0 +1,17 @@
+package dnaaaaahtac.wooriforei.domain.auth.controller;
+
+import dnaaaaahtac.wooriforei.global.common.CommonResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AwsController {
+
+    @GetMapping("/aws")
+    public ResponseEntity<CommonResponse<Void>> awsfucking() {
+
+        return ResponseEntity.ok().body(CommonResponse.of("awsfucking", null));
+    }
+
+}
