7f9e39baa3801e8cda9fea3dde8fa13fa75151c6
김은채 <ke808762@gmail.com>
2024-04-29 22:21:41 +0900

    fix: cors 수정

--- src/main/java/dnaaaaahtac/wooriforei/domain/auth/controller/AwsController.java
+++ src/main/java/dnaaaaahtac/wooriforei/domain/auth/controller/AwsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class AwsController {
 
     @GetMapping("/aws")
