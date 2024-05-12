5be6470fccf0a45ba61fd412b8651cddb173df9d
de123456sdf <cdaeun95@gmail.com>
2024-05-12 23:06:39 +0900

    fix: cors 수정

--- src/main/java/dnaaaaahtac/wooriforei/global/config/CustomCorsFilter.java
+++ src/main/java/dnaaaaahtac/wooriforei/global/config/CustomCorsFilter.java
@@ -13,14 +13,23 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class CustomCorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {  // 예외를 메서드 시그니처에 포함
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-        response.addHeader("Access-Control-Allow-Credentials", "true");
+            throws ServletException, IOException {
+        // Set CORS headers only if not already set
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        }
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        }
+        if (response.getHeader("Access-Control-Allow-Credentials") == null) {
+            response.addHeader("Access-Control-Allow-Credentials", "true");
+        }
 
         try {
-            filterChain.doFilter(request, response);  // 요청과 응답을 다음 필터로 전달
+            filterChain.doFilter(request, response);
         } catch (java.io.IOException e) {
             throw new RuntimeException(e);
         }
